2743c519d53768f335db6f31dcb9d4b155ccd893
Siyamukelo Cebekulu <siyamukelocebekulu@gmail.com>
2026-02-05 12:42:49 +0200

    Data and DTO

---
+++ TodoApi/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<TodoItem> Todos => Set<TodoItem>();
+}
